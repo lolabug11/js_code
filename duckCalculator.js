7b1383c2a70b6b0f49e58d9d1c60c45e40719391
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2026-02-13 20:13:55 +0000

    Comit

--- duckCalculator.js
+++ duckCalculator.js
@@ -1,18 +1,17 @@
-let ducksPerCol = 6
-let ducksPerRow = 10
+let baseCol = 6
+let baseRow = 10
+let screenWidth = 160
+let screenHeight = 120
 let duckPositionX = 8
 let duckPositionY = 8
 let numberOfDucks = 70
-// for (let i = 1; i < 71; i+=1) {
-//     console.log(i)
-// }
-// for (let x = ducksPerCol; x > 0; x--) {
-//     for (let y = ducksPerRow; y > 0; y--) {
-//     console.log(y)
-//     }
-// }
 let duckY = duckPositionY
 let duckX = duckPositionX
+let n = Math.ceil(Math.log2(numberOfDucks/70))
+let scale = 2**-2
+let cellHight = scale * 16
+let cellWidth = scale * 16
+
 for (let a = numberOfDucks; a > 0; a) {
     for (let y = ducksPerCol; y > 0; y--) {
         for (let x = ducksPerRow; x > 0; x--) {
---
+++ test.js
@@ -0,0 +1,21 @@
+let scale = 1
+let defaultAmount = 70
+let amountOfDucks = 71
+let col = 6
+let row = 10
+
+
+    scale = (defaultAmount / amountOfDucks)
+    if (Number.isInteger(col / scale)) {
+        if (Number.isInteger(row / scale)) {
+            col = col / scale
+            row = row / scale
+        } else {
+            
+        }
+
+
+    } else {
+        
+    }
+
